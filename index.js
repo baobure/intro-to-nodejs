027bb65e3df5cac998cd56cc8b614afb19df79d3
Blastiq <blastiquemajar@gmail.com>
2022-08-07 19:15:52 +0100

    Add files via upload
    Superhero and Supervillains packages implimentation

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Super Heroes
+
+var superheroes = require("superheroes");
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName);
+
+
+//SuperVillains
+
+
+var supervillains = require("supervillains");
+
+var mySuperVillains = supervillains.random();
+
+console.log(mySuperVillains);
